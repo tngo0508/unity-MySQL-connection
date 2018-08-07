b4536036b14b78a02feee272224517fe7834c797
Thomas Ngo <tngo0508@gmail.com>
2018-08-07 12:45:19 -0700

    update score in DB

---
+++ php-script/savedata.php
@@ -0,0 +1,27 @@
+<?php
+  $con = mysqli_connect('localhost', 'root', '', 'unityaccess');
+
+  if (mysqli_connect_errno()) {
+    echo "1: Connection failed"; //error code#1 = connection fail
+    exit();
+  }
+
+  $username = $_POST["name"];
+  $newScore = $_POST["score"];
+
+  //double check there is only one user with this name
+  $nameCheckQuery = "SELECT username FROM players WHERE username='".$username."';";
+
+  $nameCheck = mysqli_query($con, $nameCheckQuery) or die("2: Name check query failed"); //error code#2 - username check query failed
+
+  if (mysqli_num_rows($nameCheck) != 1) {
+    echo "5: Either no user with name, or more than one"; //error code #5 - number of names matching != 1
+    exit();
+  }
+
+  $updatequery = "UPDATE players SET score =".$newScore." WHERE username = '".$username."';";
+  mysqli_query($con, $updatequery) or die("7: Save query failed"); //error code #7 - UPDATE query failed
+
+  echo "0";
+
+ ?>
---
+++ uinity-c#-script/Game.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+
+    public Text playerDisplay;
+    public Text scoreDisplay;
+
+    private void Awake()
+    {
+        if (DBManager.username == null) {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+        playerDisplay.text = "Player: " + DBManager.username;
+        scoreDisplay.text = "Score: " + DBManager.score;
+    }
+
+    public void CallSaveData()
+    {
+        StartCoroutine(SavePlayerData());
+    }
+
+    IEnumerator SavePlayerData()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", DBManager.username);
+        form.AddField("score", DBManager.score);
+
+        WWW www = new WWW("http://localhost/sqlconnect/savedata.php", form);
+        yield return www;
+        if (www.text == "0")
+        {
+            Debug.Log("Game Saved.");
+        }
+        else
+        {
+            Debug.Log("Save failed. Error #" + www.text);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+    }
+
+    public void IncreaseSCore()
+    {
+        DBManager.score++;
+        scoreDisplay.text = "Score: " + DBManager.score;
+
+    }
+}
